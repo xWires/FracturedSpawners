878a3112a6888f7a2a372396d45cd0bd75241064
xWires <xwires@tangledwires.xyz>
2024-04-26 00:06:46 +0100

    Add methods allowing the creation of spawners with predetermined EntityTypes

--- src/main/java/xyz/tangledwires/fracturedspawners/SpawnerItems.java
+++ src/main/java/xyz/tangledwires/fracturedspawners/SpawnerItems.java
@@ -1,13 +1,21 @@
 package xyz.tangledwires.fracturedspawners;
 
 import org.bukkit.Material;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import net.md_5.bungee.api.ChatColor;
 import xyz.tangledwires.fracturedspawners.util.PersistantDataContainerUtils;
 
 public class SpawnerItems {
+    /**
+     * Gets an ItemStack representing a blank fractured spawner.
+     * 
+     * @return An ItemStack representing a blank fractured spawner.
+     */
     public static ItemStack getFracturedSpawner() {
         ItemStack stack = new ItemStack(Material.SPAWNER);
         ItemMeta meta = stack.getItemMeta();
@@ -16,6 +24,28 @@ public class SpawnerItems {
         PersistantDataContainerUtils.setIsFracturedSpawner(stack, true);
         return stack;
     }
+    /**
+     * Gets an ItemStack describing a fractured spawner with the provided EntityType.
+     * 
+     * @param et The EntityType that should be contained in this spawner.
+     * @return An ItemStack representing a fractured spawner with the specified EntityType.
+     */
+    public static ItemStack getFracturedSpawner(EntityType et) {
+        ItemStack is = getFracturedSpawner();
+        BlockStateMeta bsm = (BlockStateMeta) is.getItemMeta();
+        CreatureSpawner cs = (CreatureSpawner) bsm.getBlockState();
+
+        cs.setSpawnedType(et);
+        bsm.setBlockState(cs);
+        is.setItemMeta(bsm);
+
+        return is;
+    }
+    /**
+     * Gets an ItemStack representing a blank repaired spawner.
+     * 
+     * @return An ItemStack representing a blank repaired spawner.
+     */
     public static ItemStack getRepairedSpawner() {
         ItemStack stack = new ItemStack(Material.SPAWNER);
         ItemMeta meta = stack.getItemMeta();
@@ -23,5 +53,22 @@ public class SpawnerItems {
         stack.setItemMeta(meta);
         PersistantDataContainerUtils.setIsRepairedSpawner(stack, true);
         return stack;
+    }
+    /**
+     * Gets an ItemStack describing a repaired spawner with the provided EntityType.
+     * 
+     * @param et The EntityType that should be contained in this spawner.
+     * @return An ItemStack representing a repaired spawner with the specified EntityType.
+     */
+    public static ItemStack getRepairedSpawner(EntityType et) {
+        ItemStack is = getRepairedSpawner();
+        BlockStateMeta bsm = (BlockStateMeta) is.getItemMeta();
+        CreatureSpawner cs = (CreatureSpawner) bsm.getBlockState();
+
+        cs.setSpawnedType(et);
+        bsm.setBlockState(cs);
+        is.setItemMeta(bsm);
+
+        return is;
     }
 }
